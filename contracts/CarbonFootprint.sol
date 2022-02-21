9079d8a4ba36ea7b5a2227ee7c8e2983433f5e1d
Alessandro Mele <64039038+AlessandroMele@users.noreply.github.com>
2022-02-21 19:04:32 +0100

    Add files via upload

---
+++ contracts/CarbonFootprint.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity>=0.5.16;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract CarbonFootprint is ERC721 {
+
+
+
+    // per tenere il conteggio dei token prodotti fino ad ora (e quindi delle materie prima/prodotti nel catalogo)
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // struttura che contiene le informazioni della risorsa
+    struct Risorsa {
+        string nome; // nome risorsa
+        uint256 lotto; // lotto che identifica la risorsa (inserito dall'utente)
+        string[] nomi_attivita; // vettore che contiene i nomi delle attività svolte per produrre la risorsa (se presenti)
+        uint32[] valori_CO2_attivita; // vettore che contiene i consumi delle attività svolte per produrre la risorsa (se presenti)
+        uint32 valore_CO2; // valore di CO2 della risorsa
+        string tipologia; // tipologia di risorsa (materia prima, prodotto o utilizzata)
+        uint256[] lotti_materie_prime; // materie prime utilizzate per produrre la risorsa (se presenti)
+        uint256 token; // token che identifica la risorsa (prodotto nel momento in cui la risorsa è inserita nella bc)
+        bool exists; // attributo che serve per capire se la risorsa esiste o meno
+    }
+
+    
+    mapping(uint256=>Risorsa) private risorsaByLotto; // sapendo il lotto ricavo la risorsa
+    mapping(uint256=>Risorsa) private risorsaByToken_prodotto; // sapendo il token ricavo la risorsa
+
+
+    // costruttore del token 
+    constructor () ERC721("CarbonFootprint", "CFP"){
+    }
+
+
+
+    // tale funzione crea la risorsa (sia materia prima che trasformato) e la inserisce nelle mapping per 
+    // poterne poi estrarre le informazioni
+    function creaProdotto(
+        uint256 _lotto, uint32 _valore_CO2, string[] memory _nomi_attivita, 
+        uint32[] memory _valori_CO2_attivita, string memory _tipologia, 
+        address _possessore, uint256[] memory _lotti_materie_prime, string memory _nome
+        )
+        public returns(uint256){
+
+        // il token della risorsa è uguale al token corrente + 1
+        _tokenIds.increment();
+        uint256 id_nuovo_prodotto = _tokenIds.current();
+
+        // creo un'istanza di Risorsa 
+        Risorsa memory risorsa = Risorsa(
+            _nome,
+            _lotto,
+            _nomi_attivita,
+            _valori_CO2_attivita,
+            _valore_CO2,
+            _tipologia,
+            _lotti_materie_prime,
+            id_nuovo_prodotto,
+            true
+        );
+
+        // inserisco l'istanza di risorsa nei due mapping (conoscendone il lotto ed il token)
+        risorsaByLotto[_lotto] = risorsa;
+        risorsaByToken_prodotto[id_nuovo_prodotto] = risorsa;
+        // assegno la risorsa al possessore
+        _mint(_possessore, id_nuovo_prodotto);
+
+        return id_nuovo_prodotto;
+    }
+
+
+
+    // funzione che riceve un lotto e contrassegna la relativa risorsa come "utilizzata"
+    function setTipologiaUtilizzato(
+        uint256 _lotto
+    ) external {
+
+        // setto la risorsa come utilizzata in entrambi i mapping
+        risorsaByLotto[_lotto].tipologia = "utilizzata";
+        uint256 token = risorsaByLotto[_lotto].token;
+        risorsaByToken_prodotto[token].tipologia = "utilizzata";
+
+    }
+
+
+
+
+
+    // restituisce il numero di token prodotti fino ad ora
+    function currentToken(
+    ) external view returns(uint256) {
+
+        return _tokenIds.current();
+
+    }
+
+
+    // funzione che riceve un token e restituisce la risorsa con quel token
+    function getRisorsaByIdProdotto(
+        uint256 _token_prodotto
+    ) external view returns(Risorsa memory) {
+
+        return risorsaByToken_prodotto[_token_prodotto];
+
+    }
+
+
+
+    // funzione che riceve un lotto e restituisce la risorsa con quel lotto
+    function getRisorsaByLotto(
+        uint256 _lotto
+    ) external view returns(Risorsa memory) {
+
+        return risorsaByLotto[_lotto];
+          
+    }
+
+
+}
---
+++ contracts/Gestore_nft.sol
@@ -0,0 +1,281 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity>=0.5.16;
+
+import "./CarbonFootprint.sol";
+import "./Generica.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+
+contract Gestore_nft is Ownable {
+
+
+
+
+
+    // mapping attraverso i quali posso ricavare i produttori, trasformatori e clienti
+    mapping(uint256=>address) public produttori;
+    mapping(uint256=>address) public trasformatori;
+    mapping(uint256=>address) public clienti;
+    uint256 numero_produttori = 0;
+    uint256 numero_trasformatori = 0;
+    uint256 numero_clienti = 0;
+
+
+    // creo l'oggetto che rappresenta il token non fungibile
+    CarbonFootprint carbonFootprint = new CarbonFootprint();
+
+
+    
+
+
+    //funzione con cui il creatore dello smartContract può aggiungere un nuovo produttore, trasformatore o cliente
+    function aggiungiAttore(
+        string memory _tipo, address _indirizzo_account
+    ) external onlyOwner {
+
+        // errore se il ruolo inserito è diverso dai 3 disponibili
+        require(
+            (Generica.stringCompare(_tipo, "produttore")) || 
+            (Generica.stringCompare(_tipo, "trasformatore")) || 
+            (Generica.stringCompare(_tipo, "cliente")),
+            "I ruoli disponibili sono: _produttore_ _trasformatore_ e _cliente_"
+            );
+
+
+        // errore se l'account inserito compare già nel mapping relativo al ruolo richiesto
+        require(
+            nonEsistente(_tipo, _indirizzo_account),
+            "Account gi\xC3\xa0 presente con questo ruolo"
+            );
+
+        // in base al ruolo passato, l'account viene inserito nel corrispondente mapping
+        if(Generica.stringCompare(_tipo, "produttore")) {
+            produttori[numero_produttori] = _indirizzo_account;
+            numero_produttori++;
+        }
+
+        else if(Generica.stringCompare(_tipo, "trasformatore")) {
+            trasformatori[numero_trasformatori] = _indirizzo_account;
+            numero_trasformatori++;
+        }
+
+        else if(Generica.stringCompare(_tipo, "cliente")) {
+            clienti[numero_clienti] = _indirizzo_account;
+            numero_clienti++;
+        }
+    }
+
+
+
+
+
+    //crea una materia prima a partire da noe, valore di CO2 e lotto
+    function creaMateriaPrima(
+        uint256 _lotto, uint32 _valore_CO2, string memory _nome
+    ) external {
+
+        // errore se il richiedente non è un produttore
+        require(!nonEsistente("produttore", msg.sender), "Solo i produttori possono aggiungere materie prime");
+        // errore se il lotto inserito è 0
+        require(_lotto != 0, "Id lotto non valido");
+        // errore se i valori di CO2 non rispettano le richieste
+        require(_valore_CO2>0 && _valore_CO2<100, "Valore di CO2 non valido");
+        // errore se il lotto inserito è già presente nel catalogo
+        require(carbonFootprint.getRisorsaByLotto(_lotto).exists==false, "Id lotto gi\xC3\xa0 presente");
+        // creo 3 array vuoti, perché al "crea prodotto" devono essere passati anche le riorse usate per produrre la risorsa
+        // le attività svolte ed il loro consumo, ma poiché stiamo inserendo una materia prima, tali valori sono nulli
+        uint256[] memory vettore_risorse;
+        string[] memory nomi_attivita;
+        uint32[] memory consumi_attivita;
+
+        // creo effettivamente la materia prima
+        carbonFootprint.creaProdotto(_lotto, _valore_CO2, nomi_attivita, consumi_attivita, "materia prima", msg.sender, vettore_risorse, _nome);
+
+    }
+
+
+    // consente di trasferire una materia prima ad un altro attore
+    function trasferimentoMateriaPrima(
+        address _destinatario, uint256 _lotto
+        ) external {
+        CarbonFootprint.Risorsa memory risorsa = carbonFootprint.getRisorsaByLotto(_lotto);
+        require(getOwnerByToken(risorsa.token)==msg.sender, "Non sei in possesso della risorsa");  
+        require(!nonEsistente(_destinatario), "L'indirizzo deve appartenere ad un produttore, trasformatore o cliente");
+        carbonFootprint.transferFrom(msg.sender, _destinatario, risorsa.token);
+    }
+
+
+    // crea un nuovo prodotto a partire da un vettore di lotti di materie prime, un vettore di nomi di attività svolte
+    // un vettore di consumi delle attività svolte, un nome ed un lotto
+    function creaProdotto(
+        string[] memory _nomi_attivita, uint32[] memory _valori_CO2_attivita, string memory _nome, uint256[] memory _lotti_materie_prime, uint256 _lotto_nuovo_prodotto
+    ) external {
+
+        // errore se il lotto del prodotto da creare esiste già 
+        require(carbonFootprint.getRisorsaByLotto(_lotto_nuovo_prodotto).exists==false, "Il lotto inserito \xc3\xa8 gi\xC3\xa0 presente");
+        // errore se il richiedente non è un trasformatore
+        require(!nonEsistente("trasformatore", msg.sender), "Solo i trasformatori possono aggiungere un prodotto trasformato");
+        // inizializzo la CO2 totale a 0, poi ci sommerò quella delle attività svolte e quella delle materie prime utilizzate
+        uint32 CO2_totale = 0; 
+
+        // per ogni materia prima controllo se è valida ed utilizzabile, poi sommo i corrispondenti contributi in CO2
+        for (uint i=0;i<_lotti_materie_prime.length;i++) {
+
+            // estraggo la risorsa dal catalogo conoscendone il lotto
+            CarbonFootprint.Risorsa memory risorsa = carbonFootprint.getRisorsaByLotto(_lotti_materie_prime[i]);
+
+            // errore se il lotto è 0 
+            require(_lotti_materie_prime[i] != 0, "Il lotto 0 non \xc3\xa8 valido");
+            // errore se il richiedente non possiede la risorsa
+            require(getOwnerByToken(risorsa.token)==msg.sender, "Non sei in possesso della materia prima con lotto");
+            // errore se la risorsa non è una materia prima
+            require(Generica.stringCompare(string(risorsa.tipologia),"materia prima"),"L'elemento con lotto pari a non \xc3\xa8 una materia prima");
+
+            // settiamo il valore "tipologia" della risorsa a "utilizzata" in modo che non possa essere usata per 
+            // produrre nuovi prodotti
+            carbonFootprint.setTipologiaUtilizzato(_lotti_materie_prime[i]);
+
+            // sommo alla CO2 totale quella della materia prima corrente
+            CO2_totale+=risorsa.valore_CO2;
+
+        }
+        
+
+        // per ogni attività sommo i consumi di CO2 al valore di CO2 totale
+        for (uint i=0;i<_valori_CO2_attivita.length;i++) {  
+
+            CO2_totale += (_valori_CO2_attivita[i]);
+        }
+            
+        // creo il prodotto passando id del lotto, CO2 totale, vettore con i nomi delle attività svolte, vettore con i 
+        // consumi delle attività svolte, tipologia (prodotto trasformato), possessore (chi ha mandato il messaggio),
+        // lotti di materie prime utilizzate e nome
+        carbonFootprint.creaProdotto(_lotto_nuovo_prodotto, CO2_totale, _nomi_attivita, _valori_CO2_attivita, "prodotto trasformato", msg.sender, _lotti_materie_prime, _nome);
+
+    }
+    
+
+
+
+
+    // funzione che riceve un token e restituisce l'indirizzo del possessore
+    function getOwnerByToken(uint256 _token) public view returns(address){
+    
+    // errore se il token è 0
+    require(_token!=0, "Il token inserito non \xc3\xa8 valido"); 
+    
+    // errore se il token inserito è maggiore di quello corrente
+    require(_token<=carbonFootprint.currentToken(), "Prodotto/materia prima non esistente"); 
+    
+    return carbonFootprint.ownerOf(_token);
+    }
+
+
+
+
+
+    // funzione che riceve un token e restituisce le informazioni relative alla risorsa con quel token
+    function getInfoByToken(uint256 _token)
+        public view returns(string memory, uint256, string[] memory, uint32[] memory, uint32, string memory, uint256[] memory, uint256) {
+
+        // errore se il token passato è 0 o è maggiore del token corrente 
+        require(_token!=0 && _token<=carbonFootprint.currentToken(), "Il token inserito non \xc3\xa8 valido");
+        
+        // estraggo dal catalogo la risorsa con token pari a quello passato
+        CarbonFootprint.Risorsa memory risorsa = carbonFootprint.getRisorsaByIdProdotto(_token);
+        
+        return (risorsa.nome, risorsa.lotto, risorsa.nomi_attivita, risorsa.valori_CO2_attivita, risorsa.valore_CO2, risorsa.tipologia, risorsa.lotti_materie_prime, risorsa.token);
+    }
+
+
+    // funzione che riceve un lotto e restituisce le informazioni relative alla risorsa con quel lotto
+    function getInfoByLotto(uint256 _lotto)
+        public view returns(string memory, uint256, string[] memory, uint32[] memory, uint32, string memory, uint256[] memory, uint256) {
+
+        CarbonFootprint.Risorsa memory risorsa = carbonFootprint.getRisorsaByLotto(_lotto);
+        
+        // errore se il lotto appartiene ad una risorsa che non esiste
+        require(_lotto!=0 && risorsa.exists!=false, "Il lotto inserito non \xc3\xa8 valido");
+        
+        return (risorsa.nome, risorsa.lotto, risorsa.nomi_attivita, risorsa.valori_CO2_attivita, risorsa.valore_CO2, risorsa.tipologia, risorsa.lotti_materie_prime, risorsa.token);
+    }
+
+
+
+    // funzione che riceve un nome e restituisce una lista di informazioni relative alle risorse con quel nome
+    function getInfoByNome(string memory _nome)
+        public view returns(CarbonFootprint.Risorsa[] memory) {
+            
+        // creo un vettore di dimensione pari al numero di risorse prodotte fino ad ora
+        CarbonFootprint.Risorsa[] memory risorse = new CarbonFootprint.Risorsa[](carbonFootprint.currentToken());
+        uint256 trovati=0;
+        
+        // scorro tutto il catalogo alla ricerca di risorse con il nome passato
+        for (uint256 i=1;i<=carbonFootprint.currentToken();i++) {
+            CarbonFootprint.Risorsa memory risorsa_i = carbonFootprint.getRisorsaByIdProdotto(i);
+            // se la risorsa ha nome pari a quello passato allora aggiungo la risorsa al vettore e aggiungo uno a j
+            if(Generica.stringCompare(risorsa_i.nome,_nome)) {
+                risorse[trovati] = risorsa_i;
+                trovati++;
+            }
+        }
+
+        // errore se nessun prodotto ha il nome passato
+        require(trovati!=0, "Nessun prodotto o materia prima trovati con il nome inserito");
+
+        return risorse;
+    }  
+
+
+    //funzione che verifica che non sia già presente un account con lo stesso tipo di quello passato
+    function nonEsistente(string memory _tipo, address _indirizzo_account)
+        public view returns(bool) {
+
+        // se il tipo passato è produttore allora cerco sul mapping dei produttori
+        if(Generica.stringCompare(_tipo,"produttore")) {
+            for (uint i;i<numero_produttori;i++) {
+                if(_indirizzo_account == produttori[i]) return false;
+            }
+        }
+        // se il tipo passato è trasformatore allora cerco sul mapping dei trasformatore
+        else if(Generica.stringCompare(_tipo, "trasformatore")) {
+            for (uint i;i<numero_trasformatori;i++) {
+                if(_indirizzo_account == trasformatori[i]) return false;
+            }
+        }
+        // se il tipo passato è cliente allora cerco sul mapping dei clienti
+        else if(Generica.stringCompare(_tipo, "cliente")) {
+            for (uint i;i<numero_clienti;i++) {
+                if(_indirizzo_account == clienti[i]) return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+    // funzione che scorre tutti e tre i mapping degli attori per controllare se l'account appartiene ad un 
+    // produttore, trasformatore o destinatario
+    function nonEsistente(address _indirizzo_account)
+        private view returns(bool) {
+
+        for (uint i;i<numero_produttori;i++) {
+            if(_indirizzo_account == produttori[i]) return false;
+        }
+
+        for (uint i;i<numero_trasformatori;i++) {
+            if(_indirizzo_account == trasformatori[i]) return false;
+        }
+        
+
+        for (uint i;i<numero_clienti;i++) {
+            if(_indirizzo_account == clienti[i]) return false;
+        }
+
+        return true;
+    }
+
+}
